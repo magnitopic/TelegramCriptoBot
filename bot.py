46aaa6b419481bd64a679aadc1b60f9f17b4a935
magnitopic <magnitopic@gmail.com>
2021-12-30 22:21:44 +0000

    Stable version of bot

--- bot.py
+++ bot.py
@@ -45,30 +45,32 @@ load_dotenv(find_dotenv())
 # When using .env file use:
 # os.getenv('TOKEN')
 token = os.environ['TOKEN']  # Your bot's TOKEN
-cypto_bot = BotHandler(token)
+crypto_bot = BotHandler(token)
 
 
 def web_scraping(url):
     url = requests.get('https://finance.yahoo.com/quote/'+url.upper())
     soup = BeautifulSoup(url.content, 'html.parser')
-    result = soup.find("fin-streamer", {'class': "Fw(b) Fz(36px) Mb(-4px) D(ib)"})
+    result = soup.find(
+        "fin-streamer", {'class': "Fw(b) Fz(36px) Mb(-4px) D(ib)"})
     return result
 
 
+# Gets the requested asset and looks it up
 def get_price(asset):
     print(asset.upper())
     print("¨"*10)
-    # It first cheks if it's a cypto. Yahoo finance places -USD at the end of the URL for cyptos
+    # It first cheks if it's a crypto. Yahoo finance places -USD at the end of the URL for cryptos
     price = web_scraping(asset+'-usd')
     if price != None:
         format_result = 'Current '+asset.upper() + \
             ' price is '+price.text+" USD"
         return format_result
 
+    # If it isen't a crypto
     price = web_scraping(asset)
     if price != None:
-        format_result = 'Current '+asset.upper() + \
-            ' price is '+price.text+" USD"
+        format_result = 'Current ' + asset.upper() + ' price is ' + price.text + " USD"
         return format_result
 
     else:
@@ -76,19 +78,19 @@ def get_price(asset):
 
 
 def respondSchedule(asset, ID):
-    cypto_bot.send_message(ID, get_price(asset))
+    crypto_bot.send_message(ID, get_price(asset))
 
 
 def main():
     keep_alive()
-    new_offset = 0
-    new_program_update = False
+    new_offset, new_program_update = 0, False
     print('Bot now runing...')
 
     while True:
         schedule.run_pending()
-        all_updates = cypto_bot.get_updates(new_offset)
+        all_updates = crypto_bot.get_updates(new_offset)
 
+        # If there are any new updates
         if len(all_updates) > 0:
             for current_update in all_updates:
                 first_update_id = current_update['update_id']
@@ -101,38 +103,46 @@ def main():
                     first_chat_text = current_update['message']['text']
                     first_chat_id = current_update['message']['chat']['id']
                     first_chat_name = current_update['message']['from']['first_name']
-                    if new_program_update == True:
-                        if asset == None and get_price(first_chat_text) != "Asset does not exist.":
-                            asset = first_chat_text
+                    # We need a boolean to cheek if the incoming messages are meant for the /set_reminder command
+                    if new_program_update:
+                        # To set a reminder we need two values for our variables, the asset and the time
+                        if first_chat_text.lower() == "exit":
+                            new_program_update = False
+                            crypto_bot.send_message(
+                                first_chat_id, "Reminder canceled")
                             new_offset = first_update_id + 1
-                            cypto_bot.send_message(
-                                first_chat_id, 'At what time should I remind you? (24h format "XX:XX")')
-                        elif time == None and first_chat_text.lower() != "exit":
-                            # I use try except to see if the user is giving me an invalid time
-                            # I give it as an argument to the schedule library
-                            # If it gives an error I tell the user it's an invalid input
+                        elif asset == None:
+                            if get_price(first_chat_text) != "Asset does not exist.":
+                                asset = first_chat_text
+                                new_offset = first_update_id + 1
+                                crypto_bot.send_message(
+                                    first_chat_id, 'At what time should I remind you? (24h format "XX:XX")')
+                            else:
+                                crypto_bot.send_message(
+                                    first_chat_id, 'Invalid asset. Try again.(exit to cancel)')
+                                new_offset = first_update_id + 1
+                        elif time == None and asset != None:
+                            # I use try except to see if the user is giving an invalid time
+                            # I give the message as an argument to the schedule library
+                            # If it returns an error I tell the user it's an invalid input
                             try:
                                 schedule.every().day.at(first_chat_text).do(
                                     respondSchedule, asset=asset, ID=first_chat_id)
                             except:
-                                cypto_bot.send_message(
-                                    first_chat_id, "Invalid input. Try Again! (exit to cancel)")
+                                crypto_bot.send_message(
+                                    first_chat_id, "Invalid format. Try Again! (exit to cancel)")
                                 new_offset = first_update_id + 1
                             else:
                                 time = first_chat_text
                                 new_offset = first_update_id + 1
                                 new_program_update = False
-                                cypto_bot.send_message(
+                                crypto_bot.send_message(
                                     first_chat_id, "Reminder set! I'll give you "+asset+" price every day at "+time)
                                 print("Asset: "+asset)
                                 print("Time: "+time)
-                        else:
-                            cypto_bot.send_message(
-                                first_chat_id, "Reminder canceled!")
-                            new_program_update = False
-                            new_offset = first_update_id + 1
+                    # We need to cheek for the /set_reminder so that it dosen't get looked up like an asset
                     elif '/set_reminder' in first_chat_text:
-                        cypto_bot.send_message(
+                        crypto_bot.send_message(
                             first_chat_id, "What asset should I send you updates on?(No need to add /)")
                         new_offset = first_update_id + 1
                         new_program_update = True
@@ -144,10 +154,10 @@ def main():
                         word = first_chat_text.split()
                         for i in range(len(word)):
                             if '/' in word[i]:
-                                cypto_bot.send_message(
+                                crypto_bot.send_message(
                                     first_chat_id, get_price(word[i].lstrip("/")))
                                 new_offset = first_update_id + 1
-                    # We ignore any messages that don't have /
+                    # We ignore any messages that don't have a / in front
                     else:
                         new_offset = first_update_id + 1
 
